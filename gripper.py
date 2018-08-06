b4f49106f663b33c5d95631653e61ee8cc7942c5
ArminD93 <armin.derencz@gmail.com>
2018-08-06 18:57:57 +0200

    Initial commit

---
+++ gripper.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import RPi.GPIO as GPIO
+import servo
+from time import sleep
+import pigpio
+# -*- coding: utf-8 -*-
+GPIO.setwarnings(False)
+
+
+
+delay = 0.0055 #0.0055  #0.005 / 32 
+steps = 20*3 #20 * 32 * 8  
+
+pi = pigpio.pi()
+
+
+
+
+coil_A_1_pin = 18
+coil_A_2_pin = 4
+coil_B_1_pin = 15
+coil_B_2_pin = 14
+
+pi.set_mode(coil_A_1_pin, pigpio.OUTPUT)
+pi.set_mode(coil_A_2_pin, pigpio.OUTPUT)
+pi.set_mode(coil_B_1_pin, pigpio.OUTPUT)
+pi.set_mode(coil_B_2_pin, pigpio.OUTPUT)
+
+
+
+
+
+def setStep(w1, w2, w3, w4):
+#GPIO.output(coil_A_1_pin, w1)
+  pi.write(coil_A_1_pin, w1)
+  pi.write(coil_A_2_pin, w2)
+  pi.write(coil_B_1_pin, w3)
+  pi.write(coil_B_2_pin, w4)
+
+
+servo.SetAngle(30) 
+
+def  Gripper():
+	for i in range(0, steps):
+		setStep(0,1,1,0)
+		sleep(delay)
+		setStep(0,1,0,1)
+		sleep(delay)
+		setStep(1,0,0,1)
+		sleep(delay)
+		setStep(1,0,1,0)
+		sleep(delay)
+
+	sleep(1)#sekundy
+	servo.SetAngle(0) 	
+		
+
+	sleep(2)#sekundy
+
+	for i in range(0, steps):
+		setStep(1,0,1,0)
+		sleep(delay)
+		setStep(1,0,0,1)
+		sleep(delay)
+		setStep(0,1,0,1)
+		sleep(delay)
+		setStep(0,1,1,0)
+		sleep(delay)
+	
+	sleep(5)#sekundy
+
+
+
+	
+
+GPIO.cleanup()	
+	
+	
+	
---
+++ servo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import RPi.GPIO as GPIO
+from time import sleep
+import pigpio
+GPIO.setwarnings(False)
+
+servo = 12
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo, GPIO.OUT)
+
+
+pi = pigpio.pi()
+pwm = GPIO.PWM(servo, 50) # GPIO 12 for PWM with 50Hz
+pwm.start(0) # Initialization
+
+def SetAngle(angle):
+    duty = angle / 18 + 2
+
+    
+    pi.write(servo,1)
+
+    pwm.ChangeDutyCycle(duty)
+    
+    sleep(1)
+
+    pi.write(servo,0)
+
+    pwm.ChangeDutyCycle(duty)
+	
+
+
