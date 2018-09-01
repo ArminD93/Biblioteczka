10790d4e79da1811df2dcd6e3befb1eae464b67b
ArminD93 <armin.derencz@gmail.com>
2018-09-01 20:38:00 +0200

    Wprowadznie warunku zakończenia  i wyboru miejsca szukanego przedmiotu

--- ShortestPath/main.py
+++ ShortestPath/main.py
@@ -7,27 +7,39 @@ import direction
 import servo
 import gripper
 
-print ("Dijkstra's shortest path")
-print("")
-frm = raw_input('Podaj poczatek trasy: ')
-to = raw_input('Podaj koniec trasy: ')
-print("")
 
 
-list = dijkstra.Dijkstra(frm, to)
+frm = '1A'
 
-servo.SetAngle(35)  #open
-sleep(2)
-gripper.Gripper_Forward()
-servo.SetAngle(0)  # close
-gripper. Gripper_Reverse()
-sleep(2)
+
+answ = 0
+
+while answ != 'tak':
+
+	print ("Dijkstra's shortest path")
+	print("")
+	to = raw_input('Wskaż miejsce przedmiotu: ')
+	print("")
+	
+	list = dijkstra.Dijkstra(frm, to)
+	direction.Check_Direction(list)
+
+	servo.SetAngle(35)  #open
+	sleep(2)
+	gripper.Gripper_Forward()
+	servo.SetAngle(0)  # close
+	gripper. Gripper_Reverse()
+	sleep(2)
  
-direction.Check_Direction(list)
+	list = dijkstra.Dijkstra(to, frm)
+	direction.Check_Direction(list)
 
-sleep(2)	
-gripper.Gripper_Forward()
-servo.SetAngle(35)  #open
-gripper.Gripper_Reverse()
-sleep(2)
-servo.SetAngle(0)  #open
+	sleep(2)	
+	gripper.Gripper_Forward()
+	servo.SetAngle(35)  #open
+	gripper.Gripper_Reverse()
+	sleep(2)
+	servo.SetAngle(0)  #open
+	
+	print ("Czy zakończyć? tak/nie")
+	answ = raw_input("Odpowiedź: ")
